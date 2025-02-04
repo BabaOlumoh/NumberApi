ed93d11593b6671716124c6396e6ea53e478b5ee
BabaOlumoh <olumohbabatunde@gmail.com>
2025-02-04 23:41:35 +0000

    Fixes

--- Number-API.py
+++ Number-API.py
@@ -74,6 +74,6 @@ def classify_number():
 
 if __name__ == "__main__":
     app.run(debug=True)
-    app.run(host='0.0.0.0')
-    port = int(os.environ.get("PORT", 4000))
+    port = int(os.environ.get("PORT", 8080)) 
+    app.run(host='0.0.0.0', port=port)
     
